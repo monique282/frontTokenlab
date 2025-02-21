40be4525aa570491661d0bcccf221f33c74a5761
Monique de Souza <moniquejp16@gmail.com>
2025-02-21 15:12:38 -0300

    feat: criation contex

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login.jsx";
-
+import AuthProvider from "./contexts/contex.jsx";
 
 
 
@@ -8,14 +8,14 @@ function App() {
   return (
 
     <BrowserRouter>
-     
+      <AuthProvider>
         <Routes >
           
           <Route path='/' element={<Login />} />
           
 
         </Routes>
-     
+      </AuthProvider>
     </BrowserRouter>
   );
 }
---
+++ src/contexts/contex.jsx
@@ -0,0 +1,19 @@
+import PropTypes from 'prop-types';
+import { createContext, useState } from "react";
+
+export const AuthContext = createContext({});
+
+export default function AuthProvider({ children }) {
+    const [authToken, setAuthToken] = useState(() => { return localStorage.getItem("authToken")});   
+    return (
+        <AuthContext.Provider value={{ 
+            authToken, setAuthToken,
+             }}>
+            {children}
+        </AuthContext.Provider>
+    );
+}
+
+AuthProvider.propTypes = {
+    children: PropTypes.node.isRequired,
+};
