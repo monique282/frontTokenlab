d8e4a1877897d7139f5e517305a862c8887ae098
Monique de Souza <moniquejp16@gmail.com>
2025-02-22 14:17:43 -0300

    feat: to add confirm password

---
+++ src/components/registerComponets/ConfirmPasswordComponets.jsx
@@ -0,0 +1,28 @@
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import { GiPadlock } from "react-icons/gi";
+import { IconePassword, InputPassword, PassWord } from "../../assets/styled/registerStyled";
+
+export function ConfirmPasswordComponets({ confirmPassword, setConfirmPassword, showConfirmPassword, setShowConfirmPassword }) {
+
+    function toggleConfirmPasswordVisibility() {
+        setShowConfirmPassword(!showConfirmPassword);
+    }
+
+
+    return (
+        <PassWord>
+            <IconePassword>
+                <GiPadlock />
+            </IconePassword>
+            <InputPassword
+                type="text"
+                placeholder="Confirme a senha a senha"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+            />
+            <button type="button" onClick={toggleConfirmPasswordVisibility} style={{ background: 'none', border: 'none' }}>
+                {showConfirmPassword ? <FaEye style={{ fontSize: "30px", color: "#E837AB" }} /> : <FaEyeSlash style={{ fontSize: "30px", color: "#E837AB" }} />}
+            </button>
+        </PassWord>
+    )
+}
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -4,21 +4,26 @@ import { NameComponets } from "../components/registerComponets/NameComponets";
 import { EmailComponets } from "../components/registerComponets/EmailComponets";
 import { CpfComponets } from "../components/registerComponets/CpfComponets";
 import { PasswordComponets } from "../components/registerComponets/PasswordComponets";
+import { ConfirmPasswordComponets } from "../components/registerComponets/ConfirmPasswordComponets";
 
 export default function Register() {
-    const [name, setName] = useState("")
-    const [email, setEmail] = useState("")
-    const [cpf, setCpf] = useState("")
-    const [password, setPassword] = useState("")
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [cpf, setCpf] = useState("");
+    const [password, setPassword] = useState("");
     const [showPassword, setShowPassword] = useState(false);
+    const [confirmPassword, setConfirmPassword] = useState("");
+    const [showConfirmPassword, setShowConfirmPassword] = useState(false);
 
     return (
         <All>
             <Conteiner>
                 <NameComponets name={name} setName={setName} />
-                <EmailComponets  email={email} setEmail={setEmail} />
+                <EmailComponets email={email} setEmail={setEmail} />
                 <CpfComponets cpf={cpf} setCpf={setCpf} />
                 <PasswordComponets password={password} setPassword={setPassword} setShowPassword={setShowPassword} showPassword={showPassword} />
+                <ConfirmPasswordComponets confirmPassword={confirmPassword} setConfirmPassword={setConfirmPassword} showConfirmPassword={showConfirmPassword} setShowConfirmPassword={setShowConfirmPassword} />
+
             </Conteiner>
         </All >
     );
